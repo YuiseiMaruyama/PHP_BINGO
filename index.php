e7251e1bf4c4c3178e82748e8022cb086ef7fcee
YuiseiMaruyama <yuisei.rock@gmail.com>
2019-04-11 18:36:25 +0900

    add new file

--- index.php
+++ index.php
@@ -3,11 +3,14 @@
 <head>
     <meta charset="utf-8">
     <title>BINGO</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <div id="container">
         <table>
+            <!-- 見出し行 -->
             <tr>
+                <!-- テーブルヘッダー -->
                 <th>B</th>
                 <th>I</th>
                 <th>N</th>
