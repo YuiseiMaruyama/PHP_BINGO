7a505af1c552beb7ee336e167cbb3cfe3c5936ec
YuiseiMaruyama <yuisei.rock@gmail.com>
2019-04-12 01:35:13 +0900

    add new file****

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
+<?php
+
+// $nums[][]
+
+// B: $nums[0][1~15]
+// I: $nums[1][16~30]
+// ・・
+// G: $nums[4][61~75]
+
+//一般公
+// $num[$i][$i*15+1 ~ $i*15+15]
+
+$nums = [];
+
+for($i=0; $i<5; $i++){
+    $col = range($i*15+1, $i*15+15); //rangeは、ある範囲の整数を有する配列を作成する関数
+    shuffle($col);  //shuffleは、指定した配列をシャッフルして並び順をランダムにする関数
+    $num[$i] = array_slice($col,0,5); //array_sliceは、配列の一部を切り取る関数
+}
+
+var_dump($num);
+exit; //メッセージを出力して、現在のプログラムを終了する
+
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
